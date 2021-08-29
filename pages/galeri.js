5d5be1171e5003c07aabca80986ea503873b69ce
vartdader <ahmettanrikulu24@gmail.com>
2021-08-29 19:05:14 +0300

    final build

--- pages/galeri.js
+++ pages/galeri.js
@@ -1,18 +1,29 @@
 import Navbar from "../components/Navbar";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
+import Footer from "../components/Footer";
 
 const galeri = () => {
   return (
-    <div>
+    <div className="font-mono">
       <div className="text-gray-200 bg-black h-screen lg:h-full  w-full items-center">
         <Navbar />
-        <div className="w-70 h-full bg-black">
+
+        <div className="px-10 lg:px-40 h-full bg-black">
+          <p className="pt-32">
+            4.400m² alanda inovatif antrenman seçenekleri, havuz, sauna ve
+            jakuzi ile büyük bir wellness alanı ve daha fazlasına sahip olan
+            şehrin ilk Oxygen Republic Fitness ve Müzik Kulübü’nü Metropol
+            İstanbul’da açtık! Çeşitli antrenmanlar, canlı DJ performansları,
+            özel üye davetleri ve ilham veren tasarımı ile dünyada benzeri
+            olmayan kulübümüze seni de bekliyoruz!
+          </p>
           <Carousel
             autoPlay
             infiniteLoop
             showStatus={false}
             showThumbs={false}
+            emulateTouch={true}
             className="mt-10 lg:mt-60 lg:mx-52  "
           >
             <div>
@@ -35,6 +46,7 @@ const galeri = () => {
             </div>
           </Carousel>
         </div>
+        <Footer />
       </div>
     </div>
   );
